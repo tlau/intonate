e488bbc490c3d0169ed815cd749a4e08bac6e45e
Tessa Lau <tlau@tlau.org>
2013-06-09 15:17:24 -0700

    I don't remember

--- server2/assets/js/blab.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('app', ['ngResource']);
-
-app.controller('BlabCtrl', function($scope, $resource) {
-  $scope.name = "Tessa Lau";
-
-  $scope.blabs = $resource('/blabs',
-    {}, {findAll: {method:'GET', params: {}, isArray: true}});
-});
-
